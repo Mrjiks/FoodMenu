61a292196ec64fc31b598db346f7e35ba5a23f18
Harrison <jiksdevops@gmail.com>
2022-09-10 16:39:51 +0200

    incudles styled-components

--- src/App.js
+++ src/App.js
@@ -7,30 +7,30 @@ import items from "./data";
 const allcategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-	const [menuItems, setMenuItems] = useState(items);
-	const [categories] = useState(allcategories);
-	
-	const filterItems = (category) => {
-		if (category === "all") {
-			setMenuItems(items);
-			return;
-		}
-		const newItems = items.filter((item) => item.category === category);
-		setMenuItems(newItems);
-	};
-	
-	return (
-		<main>
-			<section className="menu section">
-				<div className="title">
-					<h2>Our menu</h2>
-					<div className="underline"></div>
-				</div>
-				<Categories filterItems={filterItems} categories={categories} />
-				<Menu items={menuItems} />
-			</section>
-		</main>
-	);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allcategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- src/Menu.js
+++ src/Menu.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-const Menu = ({items}) => {
-  
-  return <div className="section-center">
-    {
-      items&&items?.map(item=> {
-        const {id,title,img,desc, price} = item;
-        
-        
-        return (
+const Container = styled.div`
+  background: #eff3f6;
+`;
+const Menu = ({ items }) => {
+  return (
+    <Container className="section-center">
+      {items &&
+        items?.map((item) => {
+          const { id, title, img, desc, price } = item;
+
+          return (
             <article key={id} className="menu-item">
-              <img src={img} className="photo" alt={title}/>
-             
+              <img src={img} className="photo" alt={title} />
+
               <div className="item-info">
                 <header>
                   <h4>{title}</h4>
@@ -19,12 +22,11 @@ const Menu = ({items}) => {
                 </header>
                 <p className="item-text">{desc}</p>
               </div>
-              
             </article>
-        )
-        })
-    }
-  </div>;
+          );
+        })}
+    </Container>
+  );
 };
 
 export default Menu;
